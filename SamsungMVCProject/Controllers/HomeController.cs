2cb66258962fdbcb02cb738c2854f411b5e0358b
CodewithSnehalkumar <snehalkumar.nishandar01@gmail.com>
2024-09-23 20:25:14 +0530

    Add project files.

---
+++ SamsungMVCProject/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Mvc;
+using SamsungMVCProject.Models;
+using System.Diagnostics;
+
+namespace SamsungMVCProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ SamsungMVCProject/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SamsungMVCProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
